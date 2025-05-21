fe779013db8d308708365248f6541f4c740633b0
Ranny-leigh <rannyleigh06@gmail.com>
2025-05-21 10:47:19 +0100

    Create MyActivity.js

---
+++ MyActivity.js
@@ -0,0 +1,20 @@
+console.log ("hello");
+const pcChoice="scissors";
+const userChoice="rock";
+
+function getWinner() {
+    if (pcChoice===userChoice) {
+        console.log("it's a draw");
+    } else {
+        if (userChoice==="rock"&& pcChoice==="scissors") {
+        if (userChoice==="paper"&& pcChoice==="scissors");
+        if (userChoice==="scissors"&& pcChoice==="rock");
+            console.log("user is winner");
+        }
+    }
+}
+
+
+
+
+
